bf5a2a6c51efadfafae083ce1888627c92dab1ae
hungsvdut2k2 <hungsvdut@gmail.com>
2022-12-20 22:08:37 +0700

    feat: add gitignore

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,1 @@
-from .post import Post
 from .search_engine import SearchEngine
-from .yolov5 import YoloV5
--- modules/post.py
+++
@@ -1,4 +0,0 @@
-class Post:
-    def __init__(self, title, article) -> None:
-        self.title = title
-        self.article = article
